2e5cd450a077baa8f071c31292bbe4e6fb0de11e
Thomas <118174352+xino57@users.noreply.github.com>
2023-11-07 16:10:12 +0100

    Debut du projet - cours 1

---
+++ Projet/Projet/Meal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet
+{
+    internal class Meal
+    {
+        public PRODUCTYPE ProductType { get; set; }
+        public float Price { get; set; }
+
+        public Meal(PRODUCTYPE ProducType, float Price)
+        {
+            this.ProductType = ProducType;
+            this.Price = Price;
+        }
+    }
+}
---
+++ Projet/Projet/Program.cs
@@ -0,0 +1,25 @@
+﻿using Projet;
+using System;
+
+namespace MNS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           List<Meal> list = new List<Meal>();
+            list.Add(new Meal(PRODUCTYPE.magicSalad, 5f));
+            list.Add(new Meal(PRODUCTYPE.magicWrap, 8f));
+            list.Add(new Meal(PRODUCTYPE.magicBurguer, 8f));
+            list.Add(new Meal(PRODUCTYPE.magicGlace, 5f));
+            list.Add(new Meal(PRODUCTYPE.magicWrap, 8f));
+
+            Order order = new Order();
+            float total=  order.getTotalePrice();
+            Console.WriteLine("le prix est de " + total);
+
+
+
+        }
+    }
+}
