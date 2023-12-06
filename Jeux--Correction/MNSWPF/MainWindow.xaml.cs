891539218edc2d9e8c60c4836daae45611dcadcd
Thomas <118174352+xino57@users.noreply.github.com>
2023-12-06 10:23:16 +0100

    jeux--Correction

---
+++ Jeux--Correction/MNSWPF/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace MNSWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow
+    {
+        public Player Player1 { get; set; }
+        public Player Player2 { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+            Player1 = new Player(100);
+            Player2 = new Player(100);
+        }
+
+        private void player1Attack_Click(object sender, RoutedEventArgs e)
+        {
+            Player2.HP -= 10;
+        }
+
+        private void player2Attack_Click(object sender, RoutedEventArgs e)
+        {
+            Player1.HP -= 10;
+        }
+    }
+}
+
---
+++ Jeux--Correction/MNSWPF/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MNSWPF
+{
+    public class Player : INotifyPropertyChanged
+    {
+        private int hP;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public int HP
+        {
+            get { return hP; }
+            set {
+                if (hP != value)
+                {
+                    hP = value;
+                    NotifyPropertyChanged("HP");
+                }
+            }
+        }
+
+        public Player(int hP)
+        {
+            HP = hP;
+        }
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
