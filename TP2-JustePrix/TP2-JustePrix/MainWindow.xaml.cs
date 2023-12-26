03611d294df6eb8940bf138a8868828987f19a22
Thomas <118174352+xino57@users.noreply.github.com>
2023-12-26 18:31:00 +0100

    Finish TP-2
    Ajout: de la sérialisation ce tableau à chaque fois qu'il est mis à jour et le désérialisation à chaque fois qu'on relance le jeu

--- TP2-JustePrix/TP2-JustePrix/MainWindow.xaml.cs
+++ TP2-JustePrix/TP2-JustePrix/MainWindow.xaml.cs
@@ -29,17 +29,20 @@ namespace TP2_JustePrix
         public int Result { get; set; }
         public int CurPrice { get; set; }
         public List<Users> Users { get; set; }
-        public int Score{ get; set; }
+        public int Score { get; set; }
 
         public MainWindow()
         {
             InitializeComponent();
             Random random = new Random();
             MinPrice = 0;
-            MaxPrice= 100;
+            MaxPrice = 100;
             Score = 1000;
             CurPrice = random.Next(MinPrice, MaxPrice + 1);
             Users = new List<Users>();
+
+            // Charger les données du fichier JSON au démarrage
+            ReadJson();
 
             MessageBox.Show(CurPrice.ToString());
 
@@ -48,18 +51,25 @@ namespace TP2_JustePrix
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Jeux();
-            string jsonString;
-            jsonString = JsonSerializer.Serialize(Users);
-            string jsonFileName = "Myfile.json";
+
+            // Sérialisation de la liste d'utilisateurs après la mise à jour
+            string jsonString = JsonSerializer.Serialize(Users);
+            string jsonFileName = "MyFile.json";
             string path = AppDomain.CurrentDomain.BaseDirectory + jsonFileName;
-            StreamWriter sw = new StreamWriter(path);
-            sw.WriteLine(jsonString);
-            sw.Close();
+
+            try
+            {
+                File.WriteAllText(path, jsonString);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur lors de la sauvegarde des scores : " + ex.Message);
+            }
         }
 
         private void Jeux()
         {
-            int nbUser =0;
+            int nbUser = 0;
 
             if (int.TryParse(textUser.Text, out nbUser))
             {
@@ -69,7 +79,7 @@ namespace TP2_JustePrix
                 {
                     MessageBox.Show("Félicitation ! Tu as trouvé le bon prix");
                     string pseudo = UserPseudo.Text;
-                    Users.Add(new Users(pseudo,Math.Abs(Score)));
+                    Users.Add(new Users(pseudo, Math.Abs(Score)));
                     UpdateScores();
                     return;
 
@@ -97,7 +107,7 @@ namespace TP2_JustePrix
         // Fonction qui permet d'afficher les score dans la ListBox
         private void UpdateScores()
         {
-            Users =Users.OrderByDescending(u => u.Score).Take(5).ToList();
+            Users = Users.OrderByDescending(u => u.Score).Take(5).ToList();
             ListScores.ItemsSource = Users.Select(u => $"{u.Pseudo} - Ton Score: {u.Score}").ToList();
 
         }
@@ -120,17 +130,18 @@ namespace TP2_JustePrix
         }
 
 
-    private List<Users> GetBestScores()
+        private List<Users> GetBestScores()
         {
 
-         if (Users.Any()){
+            if (Users.Any())
+            {
                 int bestScore = Users.Max(u => u.Score);
                 List<Users> bestScorers = Users.Where(u => u.Score < bestScore).ToList();
                 MessageBox.Show("Le meilleurs score :" + bestScore.ToString());
                 return bestScorers;
-         }
+            }
 
-         return new List<Users>();
+            return new List<Users>();
         }
 
         private void ButtonDisplayBestScore_Click(object sender, RoutedEventArgs e)
@@ -138,23 +149,58 @@ namespace TP2_JustePrix
             GetBestScores();
         }
 
-    private void ReadJson()
+        private void ReadJson()
         {
-            string jsonstring = "";
             string jsonFileName = "MyFile.json";
             string path = AppDomain.CurrentDomain.BaseDirectory + jsonFileName;
             StreamReader sr = new StreamReader(path);
-            jsonstring = sr.ReadLine();
             sr.Close();
 
             try
             {
-                User user =
-            }
+                if (File.Exists(path))
+                {
+                    string jsonstring = File.ReadAllText(path);
 
-            UpdateScores();
+                    // désérialisation de la liste d'utilisateur 
+                    Users = JsonSerializer.Deserialize<List<Users>>(jsonstring);
+
+                    UpdateScores();
+
+                }
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Erreur lors de la lecture du fichier JSON :" + e.Message);
+            }
         }
 
-    }
+        private void ButtonRestart_Fichier_JSON(object sender, RoutedEventArgs e)
+        {
+            UpdateScores();
+            string jsonFileName = "MyFile.json";
+            string path = AppDomain.CurrentDomain.BaseDirectory + jsonFileName;
 
+            try
+            {
+                if (File.Exists(path))
+                {
+                    File.Delete(path); // permet de supprimer le fichier JSON existant
+                    File.WriteAllText(path, string.Empty);
+                    MessageBox.Show("Fichier JSON rénitialisé avec succés.");
+
+                }
+                else
+                {
+                    MessageBox.Show("Le fichier Json n'existe pas");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur lors de la rénitialisation du fichier JSON " + ex.Message);
+            }
+        }
+            
+    }
 }
